5d79001c02bbf831d598c076a38475eeb4b028e4
Bogdan Agafonov <bogdan.agafonov@gmail.com>
2015-09-27 12:05:11 +0300

    int rank

--- card.rb
+++ card.rb
@@ -1,8 +1,23 @@
 class Card
+  STRING_RANKS = {
+    'A' => 14
+    'K' => 13
+    'Q' => 12
+    'J' => 11
+    'T' => 10
+  }
   attr_reader :rank, :suite
   def initialize(rank, suite)
     @rank = rank
     @suite = suite
+  end
+
+  def int_rank
+    @int_rank ||= if @rank.kind_of? String
+      STRING_RANKS[@rank] 
+    else
+      @rank.to_i
+    end
   end
 
   def same_rank?(card)
