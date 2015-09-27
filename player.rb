19d57219a9482a4cfda9bfae9343b0ca7282cba1
gennady_novoselov <gennady@novoselov.biz>
2015-09-27 11:34:38 +0300

    class variables

--- player.rb
+++ player.rb
@@ -1,6 +1,8 @@
 require_relative 'card'
 class Player
 
+  attr_reader :me, :game_state
+
   ALL_IN_BET = 1000
   DEFAULT_BET = 200
   VERSION = "Default Ruby folding player"
@@ -9,22 +11,35 @@ class Player
   def bet_request(game_state)
     puts game_state.to_s
     @me = me(game_state)
+    @game_state = game_state
     return ALL_IN_BET if is_top_comb?
     return ALL_IN_BET if i_have_pair?
-    DEFAULT_BET
+    ALL_IN_BET
   rescue StandardError => e
     puts '!!!!!!!!!!!!!!!!!!!!!!!!!!'
     puts e
-    150
+    DEFAULT_BET
   end
 
   def showdown(game_state)
 
   end
 
+  def max_bet_plus_double_blind
+    max_bet + blind * 2
+  end
+
+  def max_bet
+
+  end
+
+  def blind
+
+  end
+
   private
 
-  def me(game_state)
+  def me
     game_state["players"].find { |p| p['name'] == 'Fish' }
   end
 
@@ -44,7 +59,7 @@ class Player
     my_cards.map { |card| card.suit }.uniq.size == 1
   end
 
-  def our_position(game_state)
+  def our_position
     game_state[:in_action]
   end
 
