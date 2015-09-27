d0192492312c27a315b0e12b28236953b9b423f6
thepry <ezvy@yandex.ru>
2015-09-27 11:06:37 +0300

    y

--- player.rb
+++ player.rb
@@ -2,10 +2,12 @@ require_relative 'card'
 class Player
 
   VERSION = "Default Ruby folding player"
+  TOP_COMBS = [['A', 'K'], ['A', 'Q'], ['A', 'J'], ['A', 'T'], ['K', 'Q'], ['K', 'J'], ['Q', 'J']]
 
   def bet_request(game_state)
     puts game_state.to_s
     @me = me(game_state)
+    return 1000500 if is_top_comb?
     1000
   rescue StandardError => e
     puts e
@@ -22,6 +24,10 @@ class Player
 
   def my_cards
     @me["hole_cards"]
+  end
+
+  def is_top_comb?
+    TOP_COMBS.include? my_cards.map{ |card| card['rank'] }
   end
 
 end
